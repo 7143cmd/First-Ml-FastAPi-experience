340e76c8de080d22361b22ad5197a0785bdcbd9f
Dmytro <pytakdima@gmail.com>
2025-07-27 11:24:10 +0300

    deployed on fastapi framework

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse, FileResponse
-from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
-import uvicorn
 from model import filter_text
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/favicon.ico", include_in_schema=False)
-async def favicon():
-    return FileResponse("static/favicon.ico")
+
+class TextInput(BaseModel):
+    text: str
+
 
 @app.get("/", response_class=HTMLResponse)
-def login_form(request: Request):
+async def get_form(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/censored")
+async def censored(input_data: TextInput):
+    return {"result": filter_text(input_data.text)}
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
