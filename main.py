cafc9d8cc920bb205fa9faf4f1a54d6104446cf8
Dmytro <pytakdima@gmail.com>
2025-07-29 22:51:59 +0300

    favicon added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from model import filter_text
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
@@ -11,6 +11,9 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+@app.get('/favicon.ico', include_in_schema=False)
+async def favicon():
+    return FileResponse("static/favicon.ico")
 
 class TextInput(BaseModel):
     text: str
